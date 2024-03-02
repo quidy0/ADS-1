3fe96d8bb62dc74c291dbd326634bc0f5ac229b1
Сергей Букарин <SerBuka2006@yandex.ru>
2024-03-02 20:07:10 +0300

    ADS 1

--- src/alg.cpp
+++ src/alg.cpp
@@ -18,23 +18,14 @@ bool checkPrime(uint64_t value) {
 uint64_t nPrime(uint64_t n) {
     int count = 0;
     int num = 2;
-    while (count < n) {
-        bool isPrime = true;
-        for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) {
-                isPrime = false;
-                break;
-            }
-        }
 
-        if (isPrime) {
+    while (count < n) {
+        if (checkPrime(num)) {
             count++;
         }
-
         if (count == n) {
             break;
         }
-
         num++;
     }
 
@@ -42,38 +33,22 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-    bool isPrime = true;
     int nextPrime = value + 1;
 
     while (true) {
-        isPrime = true;
-        for (int i = 2; i * i <= nextPrime; i++) {
-            if (nextPrime % i == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-
-        if (isPrime) {
+        if (checkPrime(nextPrime)) {
             return nextPrime;
         }
-
         nextPrime++;
     }
 }
 
 
+
 uint64_t sumPrime(uint64_t hbound) {
     int sum = 0;
     for (int i = 2; i < hbound; i++) {
-        bool isPrime = true;
-        for (int j = 2; j * j <= i; j++) {
-            if (i % j == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime) {
+        if (checkPrime(i)) {
             sum += i;
         }
     }
